b0c7c305fccab9269499124742120b5c90993768
Arya Stark <winteriscoming164@gmail.com>
2024-01-09 22:01:49 +0530

    dont know why dist exist!

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>hello</h1>");
+  res.send("<h1>hello world</h1>");
 });
 
 app.listen(1430, ()=>{
