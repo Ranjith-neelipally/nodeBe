df54403df172fb08d2a5e796bb259ea610ae309c
Ranjit Neelipally <ranjithkrn99@gmail.com>
2026-03-10 18:00:50 +0530

    delpoyment fix

--- dist/db/index.js
+++ dist/db/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,11 +15,35 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const variables_1 = require("../utils/variables");
 const URI = variables_1.MONGO_URI;
-mongoose_1.default
-    .connect(URI)
-    .then(() => {
-    console.log("connected to db");
-})
-    .catch((err) => {
-    console.log(err, "connection faield");
-});
+let cached = global.mongoose;
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+}
+function dbConnect() {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (cached.conn) {
+            return cached.conn;
+        }
+        if (!cached.promise) {
+            const opts = {
+                bufferCommands: false,
+            };
+            cached.promise = mongoose_1.default.connect(URI, opts).then((mongoose) => {
+                console.log("Connected to db");
+                return mongoose;
+            }).catch((err) => {
+                console.log(err, "connection failed");
+                throw err;
+            });
+        }
+        try {
+            cached.conn = yield cached.promise;
+        }
+        catch (e) {
+            cached.promise = null;
+            throw e;
+        }
+        return cached.conn;
+    });
+}
+exports.default = dbConnect;
--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,19 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 require("dotenv/config");
-require("./db");
 const routers_1 = require("./routers");
 const favicon_1 = require("./MiddleWare/favicon");
 const home_1 = require("./templates/home");
@@ -16,6 +24,17 @@ const errorHandler_1 = require("./MiddleWare/errorHandler");
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
+const db_1 = __importDefault(require("./db"));
+app.use((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield (0, db_1.default)();
+        next();
+    }
+    catch (error) {
+        console.error("Database connection failed", error);
+        res.status(500).json({ error: "Database connection failed" });
+    }
+}));
 app.use(requestContext_1.requestContextMiddleware);
 app.use(favicon_1.IgnoreFavIcon);
 app.get("/favicon.ico", (req, res) => res.status(204).end());
@@ -31,6 +50,9 @@ app.use("/photos", user_1.ValidateUserMiddleware, auth_1.verifyLoginToken, route
 app.use("/admin", routers_1.RefreshModalsRouter);
 app.use(errorHandler_1.globalErrorHandler);
 (0, errorHandler_1.setupProcessErrorHandlers)();
-app.listen(1430, () => {
-    console.log("listening to port and");
-});
+if (process.env.NODE_ENV !== "production") {
+    app.listen(1430, () => {
+        console.log("listening to port 1430");
+    });
+}
+exports.default = app;
