6e51f92bfd4c91388ca19202f98aed658901ca5e
ranjith <ranjithkrn99@gmail.com>
2026-09-08 22:40:51 +0530

    fixed vercel error

--- src/contoller/UserController/CreateNewUser/index.ts
+++ src/contoller/UserController/CreateNewUser/index.ts
@@ -2,6 +2,12 @@ import { CreateUser } from "src/@types/user";
 import User from "../../../modals/userModal";
 import { asyncHandler } from "../../../utils/asyncHandler";
 import { sendSuccess } from "../../../utils/apiResponse";
+import { generateToken } from "../../../utils/helpers";
+import {
+  hashEmailCode,
+  signEmailVerificationToken,
+} from "../../../utils/authTokens";
+import { sendVerificationMail } from "../../../utils/mail";
 
 
 export const CreateNewUser = asyncHandler(async (req: CreateUser, res) => {
@@ -10,12 +16,23 @@ export const CreateNewUser = asyncHandler(async (req: CreateUser, res) => {
     email,
     password,
     userName,
-    verified: true,
+    verified: false,
   });
 
-  // Email verification is temporarily disabled until transactional email is configured.
+  const token = generateToken(6);
+  const verificationToken = signEmailVerificationToken(
+    user._id.toString(),
+    await hashEmailCode(token),
+  );
+
+  await sendVerificationMail(token, {
+    name: user.userName,
+    email: user.email,
+    userId: user._id.toString(),
+  });
 
   return sendSuccess(res, {
     user_id: user._id,
-  }, 201, "Account created successfully. You can now sign in.");
+    verificationToken,
+  }, 201, "Please check your email for verification instructions.");
 });
--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,7 @@ import { AppError } from "./utils/AppError";
 import dbConnect from "./db";
 
 const app = express();
+app.set("trust proxy", 1);
 
 const productionOrigins = ["https://research-pal.com", "https://www.research-pal.com"];
 const developmentOrigins = ["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174"];
